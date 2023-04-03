165fb11b639881168b510db093af2657a4ef5a77
xiaomeng9597 <49544781+xiaomeng9597@users.noreply.github.com>
2023-04-04 03:02:35 +0800

    Add files via upload

--- diy-part2-beikeyun.sh
+++ diy-part2-beikeyun.sh
@@ -10,9 +10,37 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+
+
+ rm -rf package/luci-theme-argon
+ rm -rf package/lean/luci-theme-argon
+ rm -rf themes/luci-theme-argon
+ rm -rf feeds/luci/themes/luci-theme-argon
+
+
+
+
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-app-samba4/luci-app-samba/g' package/lean/autosamba/Makefile
+
+
+
+#sed -i 's/coolsnowwolf\/packages/xiaomeng9597\/packages/g' feeds.conf.default
+sed -i 's/coolsnowwolf\/luci/xiaomeng9597\/luci/g' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/xiaomeng9597/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+
+
+
+
+
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+
+
 
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
