7630ffca66af1431e639b7835529c195cee9b90f
xiaomeng9597 <49544781+xiaomeng9597@users.noreply.github.com>
2023-04-04 00:51:04 +0800

    Add files via upload

--- diy-part1-beikeyun.sh
+++ diy-part1-beikeyun.sh
@@ -23,8 +23,6 @@ rm -rf themes/luci-theme-argon
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-
 
 
 
