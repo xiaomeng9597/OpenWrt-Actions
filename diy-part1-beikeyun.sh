e69697f5e5166201af6fad6c8de9d24b0020853c
xiaomeng9597 <49544781+xiaomeng9597@users.noreply.github.com>
2023-04-04 03:02:16 +0800

    Add files via upload

--- diy-part1-beikeyun.sh
+++ diy-part1-beikeyun.sh
@@ -18,15 +18,7 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 
-rm -rf package/lean/luci-theme-argon
-rm -rf themes/luci-theme-argon
-
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
-
-
-
-#sed -i 's/coolsnowwolf\/packages/xiaomeng9597\/packages/g' feeds.conf.default
-sed -i 's/coolsnowwolf\/luci/xiaomeng9597\/luci/g' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/xiaomeng9597/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+ rm -rf package/luci-theme-argon
+ rm -rf package/lean/luci-theme-argon
+ rm -rf themes/luci-theme-argon
+ rm -rf feeds/luci/themes/luci-theme-argon
