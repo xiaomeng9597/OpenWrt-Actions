2949c96161d143885be13a953fcbc9f45cdaa770
xiaomeng9597 <49544781+xiaomeng9597@users.noreply.github.com>
2023-04-05 02:32:23 +0800

    Add files via upload

--- diy-part2-beikeyun.sh
+++ diy-part2-beikeyun.sh
@@ -33,15 +33,17 @@ sed -i 's/luci-app-samba4/luci-app-samba/g' package/lean/autosamba/Makefile
 svn co https://github.com/thinktip/luci-theme-neobird/trunk package/luci-theme-neobird
 svn co https://github.com/Leo-Jo-My/luci-theme-opentomcat/trunk package/luci-theme-opentomcat
 
+
+
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
+
+
+
+
+
 # fix luci-theme-opentomcat dockerman icon missing
 rm -f package/luci-theme-opentomcat/files/htdocs/fonts/advancedtomato.woff
 cp $GITHUB_WORKSPACE/general/advancedtomato.woff package/luci-theme-opentomcat/files/htdocs/fonts
 sed -i 's/e025/e02c/g' package/luci-theme-opentomcat/files/htdocs/css/style.css
 sed -i 's/66CC00/00b2ee/g' package/luci-theme-opentomcat/files/htdocs/css/style.css
-
-
-
-
-
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
